413c78e8de334432a61cec43c8ba7254477cd551
hudajundi <huda.aljundi97@gmail.com>
2020-10-28 08:46:59 +0300

    lab7-edited

--- server.js
+++ server.js
@@ -12,75 +12,128 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 
-app.get('/location' , handleLocation);
+app.get('/location', handleLocation);
+app.get('/', test)
 
-
-
-function handleLocation(req,res){
-    let city = req.query.city;
-    let key= process.env.GEOCODE_API_KEY;
-    superagent.get(``) . then((data)=>{        //////NOTE:  I didn't understand how to get the link and the key 
-    let jsonObject = data.body[0];
-    console.log(jsonObject);
-    let locationObject = new Location (city, jsonObject.display_name, jsonObject.lat, jsonObject.lon)
-     res.status(200).json(locationObject);
-
-    
-}) .catch(()=>{
-    res.wend('error.....');
-});
+function test(req, res) {
+    res.send('I am Alive')
 }
 
 
 
-function Location (search_query, formatted_query, latitude, longitude){ // so the data look like the client wants 
-    this.search_query= search_query;
-    this.formatted_query=formatted_query;
-    this.latitude=latitude;
-    this.longitude=longitude;
+function handleLocation(req, res) {
+    let city = req.query.city;
+    let key = process.env.GEOCODE_API_KEY;
+    superagent.get(`https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`)
+        .then((data) => {        //////NOTE:  I didn't understand how to get the link and the key
+            console.log('data', data.body[0])
+            let jsonObject = data.body[0];
+            console.log(jsonObject);
+            let locationObject = new Location(city, jsonObject.display_name, jsonObject.lat, jsonObject.lon)
+            res.status(200).json(locationObject);
+
+
+        })
+        .catch(e => {
+            res.send('error.....', e.message);
+        });
+    console.log('testing the promise')
+}
+
+
+
+function Location(search_query, formatted_query, latitude, longitude) { // so the data look like the client wants 
+    this.search_query = search_query;
+    this.formatted_query = formatted_query;
+    this.latitude = latitude;
+    this.longitude = longitude;
 }
 
 
 /////////weather//////
-app.get('/weather',handleWeather);
+app.get('/weather', handleWeather);
 
 
-function handleWeather(request,response){
+function handleWeather(req, res) {
 
-    let arrOfDays=[];
+    let city = req.query.city;
+    let keyWeth = process.env.WETHCODE_API_KEY;
 
-    try{
-        let jsonData = require('./data/weather.json');
+    let arrOfDays = [];
 
-        let JsonWatherData = jsonData.data; 
+    superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${keyWeth}`)
+        .then((data) => {  
 
-        JsonWatherData.forEach((value) =>{
-         let weatherObject = new Weather(value.weather.description,value.valid_date);
-         arrOfDays.push(weatherObject);
-        });
+    let JsonWatherObject = data.body[0];
+
+      JsonWatherData.forEach((value) => {
+          let weatherObject = new Weather(value.weather.description, value.valid_date);
+          arrOfDays.push(weatherObject);
+       });
 
         //////// to add .map/////
 
         // response.status(200).send(arrOfDays);          /// we use instead:
-        response.status(200).json(wetherdata.body.data.map(); 
-        
-        
-    }
-    catch(error){
-        response.status(500).send('Sorry, something went wrong');
-    }
+        // response.status(200).json(wetherdata.body.data.map(arrOfDays));
+
+
+    });
 }
 
 
-function Weather(forecast,time){ 
-    this.forecast = forecast; 
+function Weather(forecast, time) {
+    this.forecast = forecast;
 
-    this.time = new Date(time).toDateString() ; 
+    this.time = new Date(time).toDateString();
 }
+/////////trail///////
+app.get('/trails',handleTrail);
+function Trail(tObject) {
+    
+        this.name=tObject.name;
+        this.location=tObject.location;
+        this.length=tObject.length;
+        this.stars=tObject.stars;
+        this.star_votes=tObject.starVotes;
+        this.summary=tObject.summary;
+        this.trail_url=tObject.url;
+        this.conditions=tObject.conditionStatus;
+        this.condition_date=tObject.conditionDate.slice(0,10);
+        this.condition_time=tObject.conditionDate.slice(11);
+    }
 
+function handleTrail(req,res) {
+    let keyT = process.env.TRAIL_API_KEY;
+    let lato=req.query.latitude;
+    let long=req.query.longitude;
+    
 
+    superagent.get(`https://www.hikingproject.com/data/get-trails?lat=${lato}&lon=${long}&maxDistance=200&key=${keyT}`)
+    .then(data =>{
+        let JsonTrailObject = data.body.trails;
+         
 
-app.listen(PORT,()=>{
-    console.log(`app is listening on port ${PORT} `);
+    //   JsonWatherData.forEach((value) => {
+    //       let weatherObject = new Weather(value.weather.description, value.valid_date);
+    //       arrOfDays.push(weatherObject);
+    //    });
+
+        
+        let value=JsonTrailObject.map(element=>{
+            return new Trail(element);
+        }).catch(()=>{
+            res.status(500).send('sorry,,, wrong')
+        })
+     
 });
+}
+// trial cons: 
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
 
